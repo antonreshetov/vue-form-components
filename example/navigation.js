49485e4766aa909bc1974267a1adfb9174a8868b
Anton Reshetov <reshetov.art@gmail.com>
2018-10-02 09:10:24 +0300

    Docs: Add navigation

---
+++ example/navigation.js
@@ -0,0 +1,34 @@
+export default {
+  development: [
+    {
+      title: 'Install',
+      path: '/components/install'
+    }
+  ],
+  components: [
+    {
+      title: 'Input',
+      path: '/components/input'
+    },
+    {
+      title: 'Select',
+      path: '/components/select'
+    },
+    {
+      title: 'Checkbox',
+      path: '/components/checkbox'
+    },
+    {
+      title: 'Radio',
+      path: '/components/radio'
+    },
+    {
+      title: 'Button',
+      path: '/components/button'
+    },
+    {
+      title: 'Form',
+      path: '/components/form'
+    }
+  ]
+}
