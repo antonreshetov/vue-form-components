dcd243cb66409e4517fd93e0005f27c80ed03edd
Anton Reshetov <reshetov.art@gmail.com>
2018-10-02 09:09:04 +0300

    Docs: Add router

---
+++ example/router.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import Page from './views/Page.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  linkActiveClass: 'active',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/components/:component',
+      component: Page,
+      meta: 'docs'
+    },
+    {
+      path: '/changelog',
+      component: Page
+    }
+  ]
+})
