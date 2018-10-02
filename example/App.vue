fe8a77c941e300d5c7fa1c9c871a55d0b7959348
Anton Reshetov <reshetov.art@gmail.com>
2018-10-02 09:09:43 +0300

    Docs: Add main component

---
+++ example/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import nav from './navigation'
+
+export default {
+  name: 'App',
+
+  data () {
+    return {
+      nav
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import './assets/main.scss'
+</style>
