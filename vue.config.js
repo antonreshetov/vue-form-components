679df3760c6662b779185a00ecb3a1625e6640ad
Anton Reshetov <reshetov.art@gmail.com>
2018-10-05 12:06:03 +0300

    Add: Config different entry

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,34 @@
+const isProd =
+  process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'docs'
+const isDev = process.env.NODE_ENV === 'development'
+
 module.exports = {
-  baseUrl:
-    process.env.NODE_ENV === 'production' ? '/vue-form-components/' : '/',
-  pages: {
-    index: {
-      entry: 'example/main.js',
-      template: 'example/index.html',
-      filename: 'index.html'
+  baseUrl: isProd ? '/vue-form-components/' : '/',
+
+  chainWebpack: config => {
+    config.entryPoints.delete('app')
+
+    if (isProd) {
+      config
+        .entry('docs')
+        .add('./example/main.js')
+        .end()
+        .plugin('html')
+        .tap(args => {
+          args[0].template = './example/index.html'
+          return args
+        })
+    }
+    if (isDev) {
+      config
+        .entry('dev')
+        .add('./dev/main.js')
+        .end()
+        .plugin('html')
+        .tap(args => {
+          args[0].template = './dev/index.html'
+          return args
+        })
     }
   }
 }
